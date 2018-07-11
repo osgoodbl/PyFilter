a500c44502e75f7d4f0dff2e8e6d88d7b056da48
Brian Osgood <osgood24b@gmail.com>
2018-07-11 15:41:48 -0700

    Initialization
    First File posting

---
+++ Twitter/scraper.py
@@ -0,0 +1,48 @@
+import settings
+import tweepy
+import sys
+import csv
+import json
+
+
+class CustomStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        if hasattr(status,'retweeted_status') or status.author.screen_name == 'tf_and_huracans':
+            return True
+        elif hasattr(status,'extended_tweet'):
+            print(status.author.screen_name, status.created_at, status.text)
+            # Writing status data
+            with open('OutputStreaming.csv', 'a') as f:
+                writer = csv.writer(f, delimiter = str(","))
+                writer.writerow([status.author.screen_name, status.id_str, status.created_at, str(status.text), str(status.entities), str(status.extended_tweet)])
+        else:
+            print(status.author.screen_name, status.created_at, status.text)
+            # Writing status data
+            with open('OutputStreaming.csv', 'a') as f:
+                writer = csv.writer(f, delimiter = str(","))
+                writer.writerow([status.author.screen_name, status.id_str, status.created_at, str(status.text), str(status.entities), {"No Extended Entities":"Empty"} ])
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True  # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True  # Don't kill the stream
+
+    # Writing csv titles
+
+
+with open('OutputStreaming.csv', 'w') as f:
+    writer = csv.writer(f, delimiter = str(","))
+    writer.writerow(['Author', 'Id', 'Date', 'Text', 'Entities', 'Extended Entities'])
+
+
+auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
+auth.set_access_token(settings.access_token, settings.access_token_secret)
+api = tweepy.API(auth,timeout=200)
+
+stream_listener = CustomStreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream.filter(track=settings.TRACK_TERMS, encoding='utf-8')
---
+++ Twitter/settings.py
@@ -0,0 +1,9 @@
+TRACK_TERMS = ["#Huracan", "#Lamborghini"]
+CONNECTION_STRING = ""
+CSV_NAME = "huracan.csv"
+TABLE_NAME = "huracan"
+
+try:
+    from private import *
+except Exception:
+    pass
